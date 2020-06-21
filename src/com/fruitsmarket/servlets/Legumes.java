3781bb844de81c6f421cb31e853934aced0bd270
Nassim <nassimkissi@icloud.com>
2020-06-22 00:24:32 +0200

    Ajout de la page legumes + dynamisme de celle ci

---
+++ src/com/fruitsmarket/servlets/Legumes.java
@@ -0,0 +1,46 @@
+package com.fruitsmarket.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fruitsmarket.bdd.LegumesBDD;
+
+/**
+ * Servlet implementation class Legumes
+ */
+@WebServlet("/Legumes")
+public class Legumes extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Legumes() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/*
+		 * Recuperation des produits par méthode GET 
+		 */
+		LegumesBDD tableFruits = new LegumesBDD(); 
+		request.setAttribute("productsLegumes", tableFruits.recupererFruits());
+		this.getServletContext().getRequestDispatcher("/WEB-INF/legumes.jsp").forward(request, response); 
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
